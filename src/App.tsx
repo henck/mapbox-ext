565059d693563e5b8609307c0bc6ecf630da3f57
henck <alex.vanoostenrijk@gmail.com>
2022-12-04 18:25:40 +0000

    Add test app

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import styled from 'styled-components';
+import { createRoot } from 'react-dom/client';
+import { MapView } from './MapView';
+
+interface IProps {
+  className?: string;
+}
+
+class AppBase extends React.Component<IProps> {
+  render = () => {
+    const p = this.props;
+    return (
+      <div className={p.className}><MapView/></div>
+    );
+  }
+}
+
+const App = styled(AppBase)`
+  width: 100vw;
+  height: 100vh;
+`
+
+const root = createRoot(document.getElementById('root'));
+root.render(<App/>);
+
+// Whenever webpack rebuilds the project, refresh the browser.
+declare let module: any;
+if (module.hot) {
+  module.hot.accept(); 
+}
