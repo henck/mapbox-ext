562eb736f37043f710b6a9e8481602c646b9f3c9
henck <alex.vanoostenrijk@gmail.com>
2022-12-05 14:45:00 +0000

    Add publish batch file

---
+++ pub.bat
@@ -0,0 +1,9 @@
+rem Publish package to NPM, eliminating this /dist directory (publish only its contents)
+rem This is done by copying package.json and README.md to dist, then editing package.json
+rem to remove the /dist prefix.
+copy package.json dist
+copy README.md dist
+del dist\main.js
+del dist\index.js
+del dist\index.d.ts
+node node_modules\replace-in-file\bin\cli.js /dist/g "" dist/package.json --isRegex
