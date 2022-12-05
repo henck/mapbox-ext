d3207a3f1ecf2dc18090952ad57fac3647bd22c7
henck <alex.vanoostenrijk@gmail.com>
2022-12-05 10:34:23 +0000

    Add anchor, size, color to AnimatedLoader. Add opacity variation.

--- src/controls/AnimatedLoader.tsx
+++ src/controls/AnimatedLoader.tsx
@@ -1,9 +1,19 @@
 import * as React from 'react';
-import styled, { keyframes } from 'styled-components';
+import styled, { css, keyframes } from 'styled-components';
+
+const DEFAULT_SIZE = 40;  // Default control size (px)
 
 interface IProps {
   className?: string;
+  /** Button anchor (x,y). Negative values are offsets from the right and 
+   *  bottom. */
+  anchor: number[];  
+  /** Optional control size (px).  Defaults to 40. */
+  size?: number;
+  /** Should the loader currently be shown? */
   active: boolean;
+  /** SVG color. Defaults to white. */
+  color?: string;
 }
 
 /**
@@ -28,11 +38,16 @@ const rotate = keyframes`
   to   { transform: rotate(360deg); }
 `;
 
+const fade = keyframes`
+  0% { opacity: 1 }
+  50% { opacity: 0.2 }
+  100% { opacity: 1 }
+`;
+
 const Svg = styled('svg')`
-  width: 40px;
-  height: 40px;
-  fill: #fff;
-  animation: ${rotate} 2s linear infinite;
+  width: 100%;
+  height: 100%;
+  animation: ${rotate} 2s linear infinite, ${fade} 3s linear infinite;
 `
 
 /**
@@ -40,13 +55,21 @@ const Svg = styled('svg')`
  */
 const AnimatedLoader = styled(AnimatedLoaderBase)`
   position: absolute;
-  right: 40px;
-  top: 40px;
+  ${p => p.anchor[0] >= 0 && css`left: ${p.anchor[0]}px;`}
+  ${p => p.anchor[0] < 0 && css`right: ${-p.anchor[0]}px;`}
+  ${p => p.anchor[1] >= 0 && css`top: ${p.anchor[1]}px;`}
+  ${p => p.anchor[1] < 0 && css`bottom: ${-p.anchor[1]}px;`}
+  width: ${p => p.size ? p.size : DEFAULT_SIZE}px;
+  height: ${p => p.size ? p.size : DEFAULT_SIZE}px;
   opacity: 0;
   cursor: grab;
   pointer-events: none;
+  user-select: none;
   transition: opacity ease-in-out 150ms;
   opacity: ${p => p.active ? 1 : 0};
+  ${Svg} {
+    fill: ${p => p.color ?? "#fff"};
+  }
 `
 
 export { AnimatedLoader }
