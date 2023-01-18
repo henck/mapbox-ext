c03bdb30c295cd3ca9b6a6c252b73874e170119a
henck <alex.vanoostenrijk@gmail.com>
2023-01-18 11:55:02 +0000

    Barrel export buttons

---
+++ src/controls/buttons/index.ts
@@ -0,0 +1,4 @@
+export { CompassButton } from './CompassButton';
+export { ZoomInButton } from './ZoomInButton';
+export { ZoomOutButton } from './ZoomOutButton';
+export { MapButton, IMapButtonProps } from './MapButton';
